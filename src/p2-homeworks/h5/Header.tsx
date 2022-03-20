139d4c88f62977cb05f2f46b851398b80d430fab
Murzzik <mcalexstar@gmail.com>
2022-03-20 23:39:24 +0200

    + Routes
    Routes + navigation menu

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -3,13 +3,14 @@ import {NavLink} from "react-router-dom";
 import {PATH} from "./Routes";
 import s from './Header.module.css'
 
+
 function Header() {
     return (
         <div className={s.navigate}>
             <NavLink to={PATH.PRE_JUNIOR} className={({isActive}) => (isActive ? s.active : s.link)}>PreJunior</NavLink>
             <NavLink to={PATH.JUNIOR} className={({isActive}) => (isActive ? s.active : s.link)}>Junior</NavLink>
             <NavLink to={PATH.JUNIOR_PLUS} className={({isActive}) => (isActive ? s.active : s.link)}>Junior +</NavLink>
-            <div className={s.block}>level</div>
+            <div className={s.block}>Menu</div>
         </div>
     )
 }
