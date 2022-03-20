f1702737d14de1b1d37e7ce479712e6321822688
Murzzik <mcalexstar@gmail.com>
2022-03-20 17:09:32 +0200

    gh-pages add

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Routes";
+import s from './Header.module.css'
 
 function Header() {
     return (
         <div>
-            // add NavLinks
+            <NavLink to={PATH.PRE_JUNIOR} className={({isActive}) => (isActive ? s.active : s.link)}>PreJunior</NavLink>
+            <NavLink to={PATH.JUNIOR} className={({isActive}) => (isActive ? s.active : s.link)}>Junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS} className={({isActive}) => (isActive ? s.active : s.link)}>Junior +</NavLink>
 
         </div>
     )
